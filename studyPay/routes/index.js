b3dbd9146f068d7e4da07fc3a8ce6bad7d44e501
kritikhanal <kkritima@outlook.com>
2023-07-09 22:44:24 +0545

    About Us

--- studyPay/routes/index.js
+++ studyPay/routes/index.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var router = express.Router();
-const Users =require('../models/users');
+const Users = require("../models/users");
 
 /* GET home page. */
-router.get('/userpage',function(_req,res,_next) {
+router.get("/userpage", function (_req, res, _next) {
   Users.find().then((users) => {
-    res.render('dashboard',{
-      name:'study pay',
-      userList: users
+    res.render("dashboard", {
+      name: "study pay",
+      userList: users,
     });
   });
 });
@@ -24,83 +24,79 @@ router.get("/signupform", function (req, res, next) {
   res.render("signupform");
 });
 
-router.post("/signup", async function(req, res, next){
+router.post("/signup", async function (req, res, next) {
   // console.log(req.bonodedy)
-  debugger
+  debugger;
   let users = new Users({
-    "name": req.body.name,
-    "email" : req.body.email,
-    "password": req.body.password,
-    "contact": req.body.contact,
-    "fieldOfInterest":req.body.fieldOfInterest,
-    
-    })
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    contact: req.body.contact,
+    fieldOfInterest: req.body.fieldOfInterest,
+  });
 
-    // router.get("/search", async function(req, res, next) {
-    //   const query = req.query.q; // Get the search query from the request query parameters
-    
-    //   try {
-    //     const searchResults = await Users.find({
-    //       fieldOfInterest: { $regex: new RegExp(query, "i") }
-    //     }).exec();
-    
-    //     res.render("searchResults", { results: searchResults });
-    //   } catch (error) {
-    //     console.error(error);
-    //     res.status(500).send("An error occurred during the search.");
-    //   }
-    // });
+  router.get("/search", async function (req, res, next) {
+    const query = req.query.q; // Get the search query from the request query parameters
 
-    // router.get("/search", async function(req, res, next) {
-    //   const query = req.query.q; // Get the search query from the request query parameters
-    
-    //   try {
-    //     const searchResults = await Users.find({
-    //       fieldOfInterest: { $regex: new RegExp(query, "i") }
-    //     }).exec();
-    
-    //     res.render("mainpage", { title: "Express", results: searchResults });
-    //   } catch (error) {
-    //     console.error(error);
-    //     res.status(500).send("An error occurred during the search.");
-    //   }
-    // });
-    
-    // router.get("/search", async function(req, res, next) {
-    //   const query = req.query.q; // Get the search query from the request query parameters
-    
-    //   try {
-    //     const searchResults = await Users.find({
-    //       fieldOfInterest: { $regex: new RegExp(query, "i") }
-    //     }).exec();
-    
-    //     res.render("dashboard", { results: searchResults });
-    //   } catch (error) {
-    //     console.error(error);
-    //     res.status(500).send("An error occurred during the search.");
-    //   }
-    // });
+    try {
+      const searchResults = await Users.find({
+        fieldOfInterest: { $regex: new RegExp(query, "i") },
+      }).exec();
 
-    router.get("/search", async function(req, res, next) {
-      const query = req.query.q; // Get the search query from the request query parameters
-    
-      try {
-        const searchResults = await Users.find({
-          fieldOfInterest: { $regex: new RegExp(query, "i") }
-        }).exec();
-    
-        res.render("dashboard", { title: "Express", results: searchResults });
-      } catch (error) {
-        console.error(error);
-        res.status(500).send("An error occurred during the search.");
-      }
-    });
+      res.render("dashboard", { title: "Express", results: searchResults });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("An error occurred during the search.");
+    }
+  });
 
   await Users.insertMany(users);
-  res.redirect('/userpage')
+  res.redirect("/userpage");
   //res.render("Homepage");
-});
-   
+  // router.get("/search", async function(req, res, next) {
+  //   const query = req.query.q; // Get the search query from the request query parameters
 
-  
+  //   try {
+  //     const searchResults = await Users.find({
+  //       fieldOfInterest: { $regex: new RegExp(query, "i") }
+  //     }).exec();
+
+  //     res.render("searchResults", { results: searchResults });
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.status(500).send("An error occurred during the search.");
+  //   }
+  // });
+
+  // router.get("/search", async function(req, res, next) {
+  //   const query = req.query.q; // Get the search query from the request query parameters
+
+  //   try {
+  //     const searchResults = await Users.find({
+  //       fieldOfInterest: { $regex: new RegExp(query, "i") }
+  //     }).exec();
+
+  //     res.render("mainpage", { title: "Express", results: searchResults });
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.status(500).send("An error occurred during the search.");
+  //   }
+  // });
+
+  // router.get("/search", async function(req, res, next) {
+  //   const query = req.query.q; // Get the search query from the request query parameters
+
+  //   try {
+  //     const searchResults = await Users.find({
+  //       fieldOfInterest: { $regex: new RegExp(query, "i") }
+  //     }).exec();
+
+  //     res.render("dashboard", { results: searchResults });
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.status(500).send("An error occurred during the search.");
+  //   }
+  // });
+});
+
 module.exports = router;
