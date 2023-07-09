079024a04ed4f1392fef53389d34e602d5a21cfb
kritikhanal <kkritima@outlook.com>
2023-07-09 13:01:23 +0545

    header 1

--- studyPay/routes/index.js
+++ studyPay/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("mainpage", { title: "Express" });
+});
+
+router.get("/header1", function (req, res, next) {
+  res.render("header1");
 });
 
 module.exports = router;
